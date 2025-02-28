d09c46c625f60768ad9d42b57eb66b973f861738
nadavzeldin <52635084+nadavzeldin@users.noreply.github.com>
2025-02-28 07:51:11 +0200

    refractor the doom code

--- scripts/npc/2042003.js
+++ scripts/npc/2042003.js
@@ -1,10 +1,60 @@
-var curMap
-var stage1 = 980000100
-var stage2= 980000200
-var stage3= 980000300
-var stage4= 980000400
-var stage5= 980000500
-var stage6= 980000600
+// Stage constants
+var stage1 = 980000100;
+var stage2 = 980000200;
+var stage3 = 980000300;
+var stage4 = 980000400;
+var stage5 = 980000500;
+var stage6 = 980000600;
+
+var curMap;
+var status = -1;
+
+// Configuration for each stage that requires handling
+var stageConfig = {
+    [stage1]: {
+        nextStage: stage2,
+        spawn: function(map) {
+            const LifeFactory = Java.type('server.life.LifeFactory');
+            const Point = Java.type('java.awt.Point');
+            var boss = LifeFactory.getMonster(8500001);
+            var bossPos = new Point(201, 80);
+            map.spawnMonsterOnGroundBelow(boss, bossPos);
+        }
+    },
+    [stage3]: {
+        nextStage: stage4,
+        spawn: function(map) {
+            const LifeFactory = Java.type('server.life.LifeFactory');
+            const Point = Java.type('java.awt.Point');
+            var boss = LifeFactory.getMonster(8820001);
+            var bossPos = new Point(201, 80);
+            map.spawnMonsterOnGroundBelow(boss, bossPos);
+        }
+    },
+    [stage5]: {
+        nextStage: stage6,
+        spawn: function(map) {
+            const LifeFactory = Java.type('server.life.LifeFactory');
+            const Point = Java.type('java.awt.Point');
+            var spawnPoint = new Point(190, 80);
+            // Spawn a group of monsters via a loop
+            for (var i = 9300291; i <= 9300294; i++) {
+                map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(i), spawnPoint);
+            }
+            // Spawn additional monsters at specific points
+            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(8800000), new Point(150, 80));
+            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(8820001), new Point(50, 80));
+            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400121), new Point(50, 80));
+            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400300), new Point(50, 80));
+            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400014), new Point(50, 80));
+        }
+    }
+};
+
+// Helper function to get the map object by ID
+function getMapById(mapId) {
+    return cm.getClient().getChannelServer().getMapFactory().getMap(mapId);
+}
 
 function start() {
     status = -1;
@@ -13,89 +63,45 @@ function start() {
 }
 
 function action(mode, type, selection) {
+    // If the player cancels, dispose immediately
     if (mode < 1) {
         cm.dispose();
-    } else {
-        if (curMap == stage1) {
-            status++;
-            var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage1);
-            const LifeFactory = Java.type('server.life.LifeFactory');
-            const Point = Java.type('java.awt.Point');
-            var bossMobid = 8500001; // 8500001
-            var boss = LifeFactory.getMonster(bossMobid);
-            var bossPos = new Point(201, 80);
-            if (map.countMonsters() == 0) { //Center tower
-                if (status == 0) {
-                    cm.sendYesNo("good job completing the doom, start next wave?");
-                } else if (status == 1) {
-                    var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage2);
-                    // only spawn in next map if there are no monsters
-                    if (map.countMonsters() == 0) {
-                        map.spawnMonsterOnGroundBelow(boss, bossPos);
-                    }
-                    cm.warpParty(stage2, 0);
-                    cm.dispose();
-                }
-            } else {
-                cm.sendOk("kill all");
-                cm.dispose();
-            }
-        }
-        if (curMap == stage3) {
-            status++;
-            var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage3);
-            const LifeFactory = Java.type('server.life.LifeFactory');
-            const Point = Java.type('java.awt.Point');
-            var bossMobid = 8820001; // pinkbean
-            var boss = LifeFactory.getMonster(bossMobid);
-            var bossPos = new Point(201, 80);
-            if (map.countMonsters() == 0) {
-                if (status == 0) {
-                    cm.sendYesNo("good job completing the doom, start next wave?");
-                } else if (status == 1) {
-                    var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage4);
-                    // only spawn in next map if there are no monsters
-                    if (map.countMonsters() == 0) {
-                        map.spawnMonsterOnGroundBelow(boss, bossPos);
-                    }
-                    cm.warpParty(stage4, 0);
-                    cm.dispose();
-                }
-            } else {
-                cm.sendOk("kill all");
-                cm.dispose();
-            }
-        }
-        if (curMap == stage5) {
-            status++;
-            var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage5);
-            if (map.countMonsters() == 0) { //Center tower
-                if (status == 0) {
-                    cm.sendYesNo("good job completing the doom, start next wave?");
-                } else if (status == 1) {
-                    var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage6);
-                    const LifeFactory = Java.type('server.life.LifeFactory');
-                    const Point = Java.type('java.awt.Point');
-                    var bossPos = new Point(190, 80);
-                    // only spawn in next map if there are no monsters
-                    if (map.countMonsters() == 0) {
-                        for (var i = 9300291; i <= 9300294; i++) {
-                            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(i), bossPos);
-                        }
-                        map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(8800000), new Point(150, 80)); // zakum num 1
-                        map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(8820001), new Point(50, 80)); // pink bean
-                        map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400121), new Point(50, 80)); // female boss
-                        map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400300), new Point(50, 80)); // the boss
-                        map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9400014), new Point(50, 80)); // Black Crow
-                    }
+        return;
+    }
 
-                    cm.warpParty(stage6, 0);
-                    cm.dispose();
-                }
-            } else {
-                cm.sendOk("kill all");
-                cm.dispose();
-            }
+    // Retrieve the current map object
+    var currentMap = getMapById(curMap);
+
+    // Check if there are still monsters left; if so, prompt the player to finish them first
+    if (currentMap.countMonsters() > 0) {
+        cm.sendOk("Please kill all monsters before proceeding.");
+        cm.dispose();
+        return;
+    }
+
+    // Increment the conversation status
+    status++;
+
+    // Look up the configuration for the current stage
+    var config = stageConfig[curMap];
+    if (!config) {
+        // If no configuration exists for this map, simply dispose
+        cm.dispose();
+        return;
+    }
+
+    // At the initial status, ask the player if they're ready to start the next wave
+    if (status === 0) {
+        cm.sendYesNo("Good job completing the doom, start next wave?");
+    }
+    // On confirmation, spawn the next wave and move the party to the next map
+    else if (status === 1) {
+        var nextMap = getMapById(config.nextStage);
+        // Only spawn monsters if the next map is currently clear
+        if (nextMap.countMonsters() === 0) {
+            config.spawn(nextMap);
         }
+        cm.warpParty(config.nextStage, 0);
+        cm.dispose();
     }
 }
--- scripts/npc/2042004.js
+++ scripts/npc/2042004.js
@@ -1,11 +1,60 @@
-var curMap
-var lobby = 980000000
-var stage1 = 980000100
-var stage2= 980000200
-var stage3= 980000300
-var stage4= 980000400
-var stage5= 980000500
-var stage6= 980000600
+// Map constants
+var lobby   = 980000000;
+var stage1  = 980000100;
+var stage2  = 980000200;
+var stage3  = 980000300;
+var stage4  = 980000400;
+var stage5  = 980000500;
+var stage6  = 980000600;
+
+var curMap;
+var status = -1;
+
+// Retrieve map object by ID
+function getMapById(mapId) {
+    return cm.getClient().getChannelServer().getMapFactory().getMap(mapId);
+}
+
+var LifeFactory = Java.type('server.life.LifeFactory');
+var Point = Java.type('java.awt.Point');
+
+// Configuration for stages handled by this script
+var stageConfig = {
+    [stage2]: {
+        nextStage: stage3,
+        bossPos: new Point(201, 80),
+        message: "good job completing the doom, start next wave?",
+        spawn: function(nextMap) {
+            // Only spawn if the next map is clear of monsters
+            if (nextMap.countMonsters() === 0) {
+                for (var i = 9500172; i < 9500179; i++) {
+                    nextMap.spawnMonsterOnGroundBelow(LifeFactory.getMonster(i), this.bossPos);
+                }
+            }
+        }
+    },
+    [stage4]: {
+        nextStage: stage5,
+        bossPos: new Point(190, 80),
+        message: "good job completing the doom, start next wave?",
+        spawn: function(nextMap) {
+            if (nextMap.countMonsters() === 0) {
+                var monsterIds = [9420547, 9420548, 9420549, 9420542, 9420543, 9420544];
+                for (var i = 0; i < monsterIds.length; i++) {
+                    nextMap.spawnMonsterOnGroundBelow(LifeFactory.getMonster(monsterIds[i]), this.bossPos);
+                }
+            }
+        }
+    },
+    [stage6]: {
+        // For stage6, the "nextStage" is the lobby, and instead of spawning, we give a reward.
+        nextStage: lobby,
+        message: "WOW, you have completed the doom, Here is your reward!",
+        reward: function() {
+            cm.gainItem(4310000, 100);
+        }
+    }
+};
 
 function start() {
     status = -1;
@@ -14,84 +63,47 @@ function start() {
 }
 
 function action(mode, type, selection) {
-    const LifeFactory = Java.type('server.life.LifeFactory');
-    const Point = Java.type('java.awt.Point');
     if (mode < 1) {
         cm.dispose();
-    } else {
-        if (curMap == stage2) {
-            status++;
-            var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage2);
-            var bossPos = new Point(201, 80);
-            if (map.countMonsters() == 0) {
-                if (status == 0) {
-                    cm.sendYesNo("good job completing the doom, start next wave?");
-                } else if (status == 1) {
-                    var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage3);
-                    // only spawn in next map if there are no monsters
-                    if (map.countMonsters() == 0) {
-                        for (var i = 9500172; i < 9500179; i++) {
-                            map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(i), bossPos);
-                        }
-                    }
-                    cm.warpParty(stage3, 0);
-                    cm.dispose();
-                }
-            } else {
-                cm.sendOk("kill all");
-                cm.dispose();
-            }
-        }
+        return;
     }
-    if (curMap == stage4) {
-        status++;
-        var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage4);
-        const LifeFactory = Java.type('server.life.LifeFactory');
-        const Point = Java.type('java.awt.Point');
-        var bossPos = new Point(190, 80);
-        if (map.countMonsters() == 0) {
-            if (status == 0) {
-                cm.sendYesNo("good job completing the doom, start next wave?");
-            } else if (status == 1) {
-                var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage5);
-                // only spawn in next map if there are no monsters
-                if (map.countMonsters() == 0) {
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420547), bossPos);
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420548), bossPos);
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420549), bossPos);
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420542), bossPos);
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420543), bossPos);
-                    map.spawnMonsterOnGroundBelow(LifeFactory.getMonster(9420544), bossPos);
-                }
-                cm.warpParty(stage5, 0);
-                cm.dispose();
-            }
-        } else {
-            cm.sendOk("kill all");
-            cm.dispose();
-        }
+
+    // Check if this stage is configured for handling
+    var config = stageConfig[curMap];
+    if (!config) {
+        cm.dispose();
+        return;
     }
-    if (curMap == stage6) {
-        status++;
-        var map = cm.getClient().getChannelServer().getMapFactory().getMap(stage6);
-        if (map.countMonsters() == 0) {
-            if (status == 0) {
-                cm.sendYesNo("WOW, you have completed the doom, Here is your reward!");
-            }
-            if (status > 0) {
-                cm.gainItem(4310000, 100);
-                cm.warpParty(lobby, 0);
-                cm.dispose();
-            }
-        } else {
-            cm.sendOk("kill all");
-            cm.dispose();
+
+    // Ensure the current map is clear before proceeding
+    var currentMap = getMapById(curMap);
+    if (currentMap.countMonsters() > 0) {
+        cm.sendOk("kill all");
+        cm.dispose();
+        return;
+    }
+
+    status++;
+
+    // At first prompt, ask the player to confirm starting the next wave or reward
+    if (status === 0) {
+        cm.sendYesNo(config.message);
+        return;
+    }
+    // Upon confirmation, process the next stage or reward logic
+    else if (status === 1) {
+        // If a spawn function is provided, execute it on the next map
+        if (config.spawn) {
+            var nextMap = getMapById(config.nextStage);
+            config.spawn(nextMap);
         }
+        // If a reward function is provided, execute it (applies for stage6)
+        if (config.reward) {
+            config.reward();
+        }
+        // Warp the party to the next stage (or lobby)
+        cm.warpParty(config.nextStage, 0);
+        cm.dispose();
+        return;
     }
 }
-
-// if (cm.canHold(4310000, 1)) {
-//     cm.gainItem(4310000, 1);
-// } else {
-//     cm.getPlayer().dropMessage(1, "Have a ETC slot available for this item."); // 4310000
-// }
