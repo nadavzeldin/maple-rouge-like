fff3ca11cd75091b92b3d7589ae4a38032624335
Nadav Zeldin <52635084+nadavzeldin@users.noreply.github.com>
2025-03-22 19:27:13 +0200

    improve end game maps

--- src/main/java/server/maps/MapFactory.java
+++ src/main/java/server/maps/MapFactory.java
@@ -47,6 +47,7 @@ import java.util.List;
 
 import static constants.game.GameConstants.SPAWN_RATE;
 import static constants.id.MapId.BOSS_MAPS;
+import static constants.id.MapId.BUFFED_SPAWN_MAPS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class MapFactory {
@@ -332,7 +333,13 @@ public class MapFactory {
         }
 
         if (!BOSS_MAPS.contains(mapid)){
-            map.duplicateSpawnPoints(SPAWN_RATE);
+            int spawn_multiplayer = SPAWN_RATE;
+
+            if (BUFFED_SPAWN_MAPS.contains(mapid)){
+                spawn_multiplayer = Math.min(10, spawn_multiplayer*2);
+            }
+
+            map.duplicateSpawnPoints(spawn_multiplayer);
         }
 
         map.setBackgroundTypes(backTypes);
