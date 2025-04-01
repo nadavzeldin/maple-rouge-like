db37061896cdf5fb145d4b2822a9d557d71a5b42
nadavzeldin <52635084+nadavzeldin@users.noreply.github.com>
2025-04-01 22:49:51 +0300

    improve text (#123)

--- scripts/npc/2003.js
+++ scripts/npc/2003.js
@@ -24,20 +24,23 @@ function action(mode, type, selection) {
         if (selection == 0) {
             cm.sendOk("#eLegends Never Die#k#n\r\n\r\nPlay testing achievers which manage to reach level 200 first are:\r\n\r\n#b1. #eChrollo#n#k\r\n#b2. #eKeen#n#k\r\n#b3. #eMiguel#n#k\r\n\r\n#d#eCongrads you legends!#k#n");
             cm.dispose();
-        } else if (selection == 1) {
-            cm.sendOk("#ascend  Powers#k#n\r\n\r\n" +
-                "#dUpon reaching level 200, you have unlocked new command:#k\r\n" +
-                "#r@ascend  <option>#k\r\n\r\n" +
-                "#eAvailable Options:#n\r\n" +
-                "0 - View your active ascensions\r\n" +
-                "1 - Hoarder Ascension: Increases the drop rate for mesos and items by 5x\r\n" +
-                "2 - Resilient Ascension: Characters become immune to debuffs\r\n" +
-                "3 - Lucky Ascension: All scrolls are atleast 90% Success\r\n" +
-                "4 - Blacksmith Ascension: on merge 10% for extra buffed merged\r\n" +
-                "5 - EarlyBird Ascension: new character start with another extra bonus of 50 for each AP\r\n" +
-                "6 - Infinite Ascension: can stack! each Infinite give +2AP to level\r\n\r\n" +
-                "#ePlease Note:#n #rAscending will send your current character to jail, forever!#k");
-            cm.dispose();
+        }  else if (selection == 1) {
+                      cm.sendOk("#ascend  Powers\r\n\r\n" +
+                          "#dUpon reaching level 200, you have unlocked new command:#k\r\n" +
+                          "#r@ascend  <option>#k\r\n\r\n" +
+                          "#eAvailable Options:#n\r\n" +
+                          "0 - View your active ascensions\r\n" +
+                          "1 - Hoarder Ascension: Increases the drop rate for mesos and items by 5x\r\n" +
+                          "2 - Resilient Ascension: Characters become immune to debuffs\r\n" +
+                          "3 - Lucky Ascension: All scrolls are atleast 90% Success\r\n" +
+                          "4 - Blacksmith Ascension: on merge 10% for extra buffed merged\r\n" +
+                          "5 - EarlyBird Ascension: new character start with another extra bonus of 50 for each AP\r\n" +
+                          "6 - Infinite Ascension: can stack! each Infinite give +2AP to level\r\n" +
+                          "7 - Stylist Ascension: @style -> teleport to unique map with extra cosmetic options\r\n" +
+                          "8 - Risktaker Ascension: unlock buffed channel (3)\r\n" +
+                          "9 - Heirloom Ascension: enable use @storage from jail once per character\r\n\r\n" +
+                          "#ePlease Note:#n #rAscending will send your current character to jail, forever!#k");
+                      cm.dispose();
         }
     } else {
         cm.dispose();
